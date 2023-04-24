d89f17551f056a5895b05f5e8db6c060f0fabbde
kaandincer <kaandincer@wustl.edu>
2023-04-24 17:11:33 -0500

    Getting Search Results for Nutritional Values

--- nutrition-app/src/App.js
+++ nutrition-app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import Search from './Search';
 import AddMeal from './AddMeal';
 import Nav from './Nav';
+import About from './About';
+import { useState } from 'react';
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const searchFood = async (query) => {
+    const response = await fetch(`https://api.nutritionix.com/v1_1/search/${query}?results=0:20&fields=item_name,brand_name,nf_calories,nf_total_fat,nf_protein,nf_total_carbohydrate,nf_serving_size_unit&appId=25850a88&appKey=037a99010fc1334604cd90034de61eaa`);
+    const data = await response.json();
+    setSearchResults(data.hits);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <section>
-          <header>
-              <h1>MealMentor</h1>
-          </header>
-         <Nav></Nav>
-          <div className="main">
-            <Search></Search>
-            <AddMeal></AddMeal>
-            <div className='prevMeals'></div>
-          </div>
-        </section>
+          <h1>MealMentor</h1>
       </header>
+      <section>
+        <Nav/>
+        <About/>
+        <Search searchFood={searchFood} searchResults={searchResults}/>
+        <AddMeal/>
+    </section>
     </div>
   );
 }
--- nutrition-app/src/Search.js
+++ nutrition-app/src/Search.js
@@ -1,19 +1,83 @@
 import './Search.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function Search(props) {
-  return (
-    <div className="search">
-        <h2>Search for Foods</h2>
-        <form id="food-search-form">
-            <label for="food-search">Search for Foods:</label>
-            <input type="text" id="food-search" name="food-search"/>
-            <input type="submit" value="Search"/>
-        </form>
-        <div id="food-results">
-            {/* <!-- Display search results here --> */}
+    const [query, setQuery] = useState('');
+    const [nutrients, setNutrients] = useState(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    
+        axios.get('https://api.nutritionix.com/v1_1/search/' + query, {
+          params: {
+            results: '0:1',
+            fields: 'item_name,brand_name,nf_calories,nf_total_fat,nf_saturated_fat,nf_cholesterol,nf_sodium,nf_total_carbohydrate,nf_dietary_fiber,nf_sugars,nf_protein',
+            appId: '25850a88',
+            appKey: '037a99010fc1334604cd90034de61eaa'
+          }
+        })
+        .then((response) => {
+          const food = response.data.hits[0].fields;
+          setNutrients({
+            name: food.item_name,
+            brand: food.brand_name,
+            calories: food.nf_calories,
+            fat: food.nf_total_fat,
+            saturatedFat: food.nf_saturated_fat,
+            cholesterol: food.nf_cholesterol,
+            sodium: food.nf_sodium,
+            carbs: food.nf_total_carbohydrate,
+            fiber: food.nf_dietary_fiber,
+            sugar: food.nf_sugars,
+            protein: food.nf_protein
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      };
+    
+    //   const handleChange = (e) => {
+    //     setQuery(e.target.value);
+    //   };
+
+    return (
+        <div className="search">
+            <h2>Search for Foods</h2>
+            <form id="food-search-form" onSubmit={handleSubmit}>
+                <label htmlFor="food-search">Search for Foods:</label>
+                <input type="text" id="food-search" name="food-search" value={query} onChange={(event) => setQuery(event.target.value)}/>
+                <input type="submit" value="Search"/>
+            </form>
+            {/* <div id="food-results">
+                {props.searchResults.map((result) => (
+                    <div key={result._id}>{result.fields.item_name}</div>
+                ))}
+            </div> */}
+            {nutrients && (
+                <div id="food-results">
+                    <h3>{nutrients.name}</h3>
+                    <p>{nutrients.brand}</p>
+                    <ul>
+                        <li>Calories: {nutrients.calories}</li>
+                        <li>Total Fat: {nutrients.fat}g</li>
+                        <li>Saturated Fat: {nutrients.saturatedFat}g</li>
+                        <li>Cholesterol: {nutrients.cholesterol}mg</li>
+                        <li>Sodium: {nutrients.sodium}mg</li>
+                        <li>Total Carbohydrate: {nutrients.carbs}g</li>
+                        <li>Dietary Fiber: {nutrients.fiber}g</li>
+                        <li>Sugar: {nutrients.sugar}g</li>
+                        <li>Protein: {nutrients.protein}g</li>
+                        <li>Fat: {nutrients.fat ? nutrients.fat.toFixed(1) + "g" : "-"}</li>
+                        <li>Carbohydrates: {nutrients.carbohydrates ? nutrients.carbohydrates.toFixed(1) + "g" : "-"}</li>
+                        <li>Fiber: {nutrients.fiber ? nutrients.fiber.toFixed(1) + "g" : "-"}</li>
+                        <li>Sugar: {nutrients.sugar ? nutrients.sugar.toFixed(1) + "g" : "-"}</li>
+                    </ul>
+                </div>)}
         </div>
-    </div>
-  );
+    );
 }
 
 export default Search;
+
